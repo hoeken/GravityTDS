3d1f527e6172215fe0c1f21c68f664fae2cf36a2
Zach Hoeken <hoeken@gmail.com>
2024-11-06 22:16:28 -0500

    refactored the code to be ADC agnostic

--- GravityTDS.h
+++ GravityTDS.h
@@ -28,16 +28,12 @@ public:
   GravityTDS();
   ~GravityTDS();
 
-  void begin();  // initialization
-  void update(); // read and calculate
-  void setPin(int pin);
-  void setTemperature(
-      float temp); // set the temperature and execute temperature compensation
-  void
-  setAref(float value); // reference voltage on ADC, default 5.0V on Arduino UNO
-  void setAdcRange(float range);      // 1024 for 10bit ADC;4096 for 12bit ADC
-  void setKvalueAddress(int address); // set the EEPROM address to store the k
-                                      // value,default address:0x08
+  void begin();                    // initialization
+  void update(int reading);        // read and calculate
+  void setTemperature(float temp); // temp of water
+  void setAref(float value);       // reference voltage on ADC
+  void setAdcRange(float range);   // 1024 for 10bit ADC; 4096 for 12bit ADC
+  void setKvalue(float kvalue);    // sets our k value
   float getKvalue();
   float getTdsValue();
   float getEcValue();
@@ -47,23 +43,16 @@ private:
   float aref; // default 5.0V on Arduino UNO
   float adcRange;
   float temperature;
-  int kValueAddress; // the address of the K value stored in the EEPROM
-  char cmdReceivedBuffer[ReceivedBufferLength +
-                         1]; // store the serial cmd from the serial monitor
-  byte cmdReceivedBufferIndex;
 
-  float kValue; // k value of the probe,you can calibrate in buffer solution
-                // ,such as 706.5ppm(1413us/cm)@25^C
+  float kValue; // k value of the probe, you can calibrate in buffer solution
+                // such as 706.5ppm(1413us/cm)@25^C
   float analogValue;
   float voltage;
   float ecValue;   // before temperature compensation
   float ecValue25; // after temperature compensation
   float tdsValue;
 
-  void readKValues();
-  boolean cmdSerialDataAvailable();
-  byte cmdParse();
-  void ecCalibration(byte mode);
+  // void ecCalibration(byte mode);
 };
 
 #endif
